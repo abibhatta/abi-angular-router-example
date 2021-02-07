146c881ffe0682d26465fe68dac39d15c52ad98e
abibhatta <abi.bhattacharya@gmail.com>
2021-02-07 12:29:36 -0600

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CrisisListComponent, HeroesListComponent ],
+  imports:      [ BrowserModule, 
+  RouterModule.forRoot([
+    {path: 'crisis-list', component: CrisisListComponent},
+    {path: 'heroes-list', component: HeroesListComponent},
+    {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+  ]),
+  FormsModule ],
+  declarations: [ AppComponent, HelloComponent, CrisisListComponent, HeroesListComponent, PageNotFoundComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
